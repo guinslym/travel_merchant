42e8923d83562a03765fa2aea25a4f3fd8c593e8
Guinslym <gmond071@pcsun.ad.uottawa.ca>
2016-10-12 19:08:19 -0400

    adding an entry

--- mongex_merchant/settings.py
+++ mongex_merchant/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django_extensions',
