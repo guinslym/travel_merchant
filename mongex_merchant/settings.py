47a6bc7e78b83a57f74daaf16a664e9a49aa67b0
Guinslym <gmond071@pcsun.ad.uottawa.ca>
2016-10-12 19:31:56 -0400

    adding site id

--- mongex_merchant/settings.py
+++ mongex_merchant/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = (
     'delivrem',
 )
 
+SITE_ID = 1
+
 ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, use a different value.
 REGISTRATION_AUTO_LOGIN = True # Automatically log the user in.
 
