8b20b7043c65ffbed8bd4c6a33a546751dca5fa0
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-10-12 02:10:34 -0400

    template viewM

--- delivrem/views.py
+++ delivrem/views.py
@@ -6,6 +6,7 @@ from .models import Product
 
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.models import User
+from django.views.generic import edit, DetailView, ListView, TemplateView
 
 
 
