981f516eda440a35625eb46aa995d019598045a4
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-10-12 02:44:34 -0400

    adding registration-redux

--- mongex_merchant/settings.py
+++ mongex_merchant/settings.py
@@ -33,14 +33,18 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'registration',
     'delivrem',
 )
 
+ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True # Automatically log the user in.
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join('staticfiles')
--- mongex_merchant/urls.py
+++ mongex_merchant/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('delivrem.urls', namespace='delivrem')),
+    url(r'^accounts/', include('registration.backends.default.urls')),
 ]
